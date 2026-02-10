cd9b7638ad82cee034c66c595a75bebc81724ff3
Tom Selfin <selfint@gmail.com>
2026-02-10 19:04:26 +0200

    clear example ui

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,12 @@
 import './style.css';
 
-import appLogo from '/favicon.svg';
 import appTemplate from './app.html?raw';
 import { initPWA } from './pwa.ts';
-import { setupCounter } from './counter.ts';
-import typescriptLogo from './typescript.svg';
 
 const app = document.querySelector<HTMLDivElement>('#app');
-
 if (!app) {
     throw new Error('Missing #app element');
 }
-app.innerHTML = appTemplate
-    .replaceAll('{{appLogo}}', appLogo)
-    .replaceAll('{{typescriptLogo}}', typescriptLogo);
-
-const counterButton = document.querySelector<HTMLButtonElement>('#counter');
-
-if (!counterButton) {
-    throw new Error('Missing #counter button');
-}
-
-setupCounter(counterButton);
+app.innerHTML = appTemplate;
 
 initPWA(app);
